fe819bd4908dee96d8521293293f67a0824782fe
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-13 20:46:02 +0900

    modified rspec

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -1,12 +1,13 @@
-reqire 'rails_helper'
+require 'rails_helper'
 
-RSpec.describe 'ユーザー登録' type: :system do
-    describe 'ユーザー登録機能' do
-        context '入力情報に誤りがある場合' do
-            xit 'エラーメッセージが画面上に表示'
-        end
-        context '入力情報が正しい場合' do
-            xit 'ユーザー登録が行われること'
-        end
+RSpec.describe 'ユーザー登録', type: :system do
+  describe 'ユーザー登録機能' do
+    context '入力情報に誤りがある場合' do
+      xit 'エラーメッセージが画面上に表示されること'
     end
+
+    context '入力情報が正しい場合' do
+      xit 'ユーザー登録が行われること'
+    end
+  end
 end
