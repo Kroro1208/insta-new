16ec06f70a4bf3c405bc580c19ac2f9d8a627a66
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-14 12:13:51 +0900

    add erb-lint.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
