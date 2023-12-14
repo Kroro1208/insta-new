7d748b679d1e1dc35663ff0bcd4cc3be303811da
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-14 15:59:31 +0900

    modified factories/user.rb user_sessions_controller.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,9 +16,9 @@
 
 FactoryBot.define do
   factory :user do
-    email{ Fake::Internet.unique.email }
+    email{ Faker::Internet.unique.email }
     password{ '123456' }
     password_confimation{ '123456'}
-    username{ Fake::Name.unique.name } 
+    username{ Faker::Name.unique.name } 
   end
 end
