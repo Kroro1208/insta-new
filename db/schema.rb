5d416a89b20bd1e56f09ade77dec591a09d0cd3e
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-14 15:02:54 +0900

    bundle install annotate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_213_111_357) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_111357) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -18,6 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 20_231_213_111_357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
+
 end
