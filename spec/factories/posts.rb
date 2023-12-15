cdb973d3f6db429c7acd22d79ec3d6a52d36f75b
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-15 20:20:32 +0900

    active storage install

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
 FactoryBot.define do
     factory :post do
         body {"MyText"}
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
 reqire "rails_helper"
 
 Rspec.describe Post type: :model do
