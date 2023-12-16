39af043dca9f956f87c38d5fc309a90893b24110
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-16 22:04:04 +0900

    Rspecによる指摘事項修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,7 +11,7 @@
 #
 #  index_posts_on_user_id  (user_id)
 #
-reqire "rails_helper"
+require "rails_helper"
 
 Rspec.describe Post type: :model do
     pending "add some examples to (or delete) #{__FILE__}"
