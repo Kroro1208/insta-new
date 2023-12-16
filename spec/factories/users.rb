55773e694861bc96dbb9815545f27f81ef98a78d
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-16 23:28:20 +0900

    Rspecによる指摘事項修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -19,8 +19,8 @@
 FactoryBot.define do
   factory :user do
     email{ Faker::Internet.unique.email }
-    password{ '123456' }
-    password_confirmation{ '123456'}
+    password{ '12345678' }
+    password_confirmation{ '12345678'}
     username{ Faker::Name.unique.name } 
   end
 end
