9c0f9658c50564a1f7226f6eb84fff76ad9abd13
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-23 22:17:12 +0900

    rspec指摘事項修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'folower_id', dependent: :destroy
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'folower_id', dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'folower_id', dependent: :destroy,
+                                  inverse_of: :follower
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'folower_id', dependent: :destroy,
+                                   inverse_of: :followed
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
@@ -63,7 +65,5 @@ class User < ApplicationRecord
 
   private
 
-  def other_user
-
-  end
+  def other_user; end
 end
