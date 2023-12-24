7ba068d5f4d82e7f574afa50f7c7d4bf5578c029
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-24 21:03:15 +0900

    ログイン時に自分と自分がフォローしている人の投稿のみを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  scope :recent, ->(count=10) {order(created_at: :desc).limit(count)}
+  scope :recent, ->(count = 10) { order(created_at: :desc).limit(count) }
 
   def owner?(object)
     object.user_id == id
@@ -62,6 +62,10 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def feed
+    Post.where(user_id: following_ids << id)
   end
 
   private
