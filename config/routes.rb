da1b72c2154e8438a2a951732cf7525b5f53093b
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-24 16:13:48 +0900

    rspec 指摘事項修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # get '/health_check', to: 'health_checks#show'
-  # root to: 'samples#index'
   root to: 'posts#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -12,7 +10,7 @@ Rails.application.routes.draw do
     resources :comments, module: :posts
     resource :like, only: %i[create destroy], module: :posts
   end
-
+  
   resources :users, only: [] do
     resource :relationship, only: %i[create destroy], module: :users
   end
