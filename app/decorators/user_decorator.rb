56fc7aaf2bde5322a25ce01ff2a3e6be71ad653f
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-28 18:07:20 +0900

    rspec指摘事項修正

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,16 @@
-class UserDecrator < ApplicationDecorator
+class UserDecorator < ApplicationDecorator
   delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  # rubocop:disable Metrics/MethodLength:
   def avatar_url(size = :origin)
     return 'http://placehold.jp/150x150.png' unless avatar.attached?
 
@@ -14,4 +25,5 @@ class UserDecrator < ApplicationDecorator
     image = command ? avatar.variant(command).processed : avatar
     h.rails_storage_proxy_url(image, only_path: true)
   end
+  # rubocop:enable Metrics/MethodLength:
 end
