57e9a3cc360a38f7f6b0fd1a8325e5c0b861f750
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-29 18:18:38 +0900

    Rspec 指摘事項修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,14 +1,14 @@
 class Norification < ApplicationRecoed
-    has_many :user_notifications, dependent: :destoroy
-    has_many :users, through: :user_notifications
-    validates :title, presence: true
-    validates :url, presemce: true, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
+  has_many :user_notifications, dependent: :destoroy
+  has_many :users, through: :user_notifications
+  validates :title, presence: true
+  validates :url, presemce: true, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
 
-    def notify(*user)
-        users = user.flatten
-        user_notifications = users.pluck(user_id)map do |user_id|
-            { notification_id: id, user_id: }
-        end
-        UserNotification.insert_all(user_notifications)
+  def notify(*user)
+    users = user.flatten
+    user_notifications = users.pluck(user_id).map do |user_id|
+      { notification_id: id, user_id: }
     end
+    UserNotification.insert_all(user_notifications) # rubocop:disable Rails/SkipsModelValidations
+  end
 end
