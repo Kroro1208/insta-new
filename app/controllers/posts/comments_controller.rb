4818ac92cf82a99e181e1ed8b202ee2a8002cb07
Kroro1208 <yamagata_7580@yahoo.co.jp>
2023-12-31 21:32:09 +0900

    コメントが来た場合に投稿者に通知が来るように設定

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,22 +1,23 @@
 class Posts::CommentsController < ApplicationController
   before_action :require_login
 
-  def show
-    @comment = current_user.comments.find(params[:id])
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+    create_notifications_about_comment_to_own_post(@comment)
   end
 
   def edit
     @comment = current_user.comments.find(params[:id])
   end
 
-  def create
-    @comment = current_user.comments.build(comment_params)
-    @comment.save
-  end
-
   def update
     @comment = current_user.comments.find(params[:id])
     @comment.update(comment_params)
+  end
+
+  def show
+    @comment = current_user.comments.find(params[:id])
   end
 
   def destroy
@@ -28,5 +29,10 @@ class Posts::CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body).merge(post_id: params[:post_id])
+  end
+
+  def create_notifications_about_comment_to_own_post(comment)
+    notification = Notification.create!(title: 'あなたの投稿にコメントがありました', url: post_url(comment.post))
+    notification.notify(comment.post.user)
   end
 end
